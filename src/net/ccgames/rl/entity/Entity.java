77667ef6ed54807bde539b5232f2b5663e0b292e
enterth3r4in <believe.the.rain@gmail.com>
2016-02-01 13:14:52 -0500

    abstracted entity class

--- src/net/ccgames/rl/entity/Entity.java
+++ src/net/ccgames/rl/entity/Entity.java
@@ -5,11 +5,20 @@ public class Entity
 	private int x, y;
 	private char glyph;
 	
+	/**
+	 * Default constructor
+	 */
 	public Entity()
 	{
 		
 	}
 	
+	/**
+	 * Constructor initialization of x,y,glyph
+	 * @param initialX - Starting x position of entity, can be modified
+	 * @param initialY - Starting y position of entity, can be modified
+	 * @param glyph - semi-permanent glyph of entity
+	 */
 	public Entity(int initialX, int initialY, char glyph)
 	{
 		this.x = initialX;
@@ -17,22 +26,38 @@ public class Entity
 		this.glyph = glyph;
 	}
 	
+	/**
+	 * Returns a coordinate system of (x, y)
+	 * @return - Coordinate system as an array of ints (x, y)
+	 */
 	public int[] getEntityPosition()
 	{
 		int[] position = {x, y};
 		return position;
 	}
 	
+	/**
+	 * Using getEntityPosition() returns x coordinate
+	 * @return - single int representing x coordinate
+	 */
 	public int getEntityXPosition()
 	{
 		return getEntityPosition()[0];
 	}
 	
+	/**
+	 * Using getEntityPosition() returns y coordinate
+	 * @return - single int representing y coordinate
+	 */
 	public int getEntityYPosition()
 	{
 		return getEntityPosition()[1];
 	}
 	
+	/**
+	 * Returns character representing the glyph used to represent the entity
+	 * @return - single char representing entity glyph
+	 */
 	public char getEntityGlyph()
 	{
 		return glyph;
